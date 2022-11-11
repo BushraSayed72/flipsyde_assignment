cf15fcfbe2a3521278e99d8a9b1af6cc098d7b24
Bushrasay <bush.sayed@gmail.com>
2022-11-11 11:16:33 +0530

    spelling mistake

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -13,7 +13,7 @@ const About=() => {
       <p className="about_para">I have done Web Development course from Udemy and a self learner and I am very passionate about my work because I love what I do.</p>
       <p className="about_para">
      I have a bachelor's  and Master's degree in Arts/History from Mumbai University. </p>
-     <p className="about_para">I would Like to build my carrer in this Field and I am truely Intrested
+     <p className="about_para">I would Like to build my carrier in this Field and I am truly Interested
      and I am always energetic and eager to learn new skills.
      Seeking a challenging position in a reputable organization to expand and utilize my learning, skills and knowledge.   </p>
       
